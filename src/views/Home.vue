0296b8ed4c7897527a8d0556ca04d8bda09e502a
hhamza1 <hamzahmitou@gmail.com>
2021-09-13 19:50:34 +0300

    Added the input group to the home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <Header />
+  <SearchInputGroup />
 </template>
 
 <script>
 
 import Header from '@/components/Header.vue'
+import SearchInputGroup from '@/components/SearchInputGroup.vue'
 
 export default {
   name: 'Home',
   components: {
-    Header
+    Header,
+    SearchInputGroup
   }
 }
 </script>
