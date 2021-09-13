b6555ec6eb33c1282ee501bdaaa2560bfa1d6f70
hhamza1 <hamzahmitou@gmail.com>
2021-09-13 19:02:00 +0300

    Fixing project structure and adding Sass

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="header">
+    <h2 class="appName">devfinder</h2>
+    <ThemeButton />
+  </div>
+</template>
+
+<script>
+
+import ThemeButton from './ThemeButton.vue'
+
+export default {
+  name: 'Header',
+  components: {
+    ThemeButton
+  }
+}
+</script>
---
+++ src/components/ThemeButton.vue
@@ -0,0 +1,12 @@
+<template>
+  <a href="#" class="theme-btn">
+    <span>Dark</span>
+    <img src="../assets/images/icon-moon.svg">
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'ThemeButton'
+}
+</script>
