0b31796388cae831766a3a18afd7a4ca4cceb122
hhamza1 <hamzahmitou@gmail.com>
2021-09-14 04:04:23 +0300

    Added usercard to page

---
+++ src/components/UserCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="user-card">
+      <div class="user-card__user-id">
+        <div class="user-card__user-id--img-container">
+          <img src="../assets/images/octocat.png">
+        </div>
+        <ul class="user-card__user-id--user-info">
+          <li id="name">The Octocat</li>
+          <li id="username">@octocat</li>
+          <li id="date">Joined 25 Jan 2011</li>
+        </ul>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'UserCard'
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <Header />
   <SearchInputGroup />
+  <UserCard />
 </template>
 
 <script>
 
 import Header from '@/components/Header.vue'
 import SearchInputGroup from '@/components/SearchInputGroup.vue'
+import UserCard from '@/components/UserCard.vue'
 
 export default {
   name: 'Home',
   components: {
     Header,
-    SearchInputGroup
+    SearchInputGroup,
+    UserCard
   }
 }
 </script>
