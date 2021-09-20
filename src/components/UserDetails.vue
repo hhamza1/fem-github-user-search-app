898a46b4c7811f145e2001e7aca5b72d88bfef56
hhamza1 <hamzahmitou@gmail.com>
2021-09-20 03:10:34 +0300

    Remove bind keyword and user shortcut

--- src/components/UserDetails.vue
+++ src/components/UserDetails.vue
@@ -1,16 +1,13 @@
 <template>
   <div class="user-details">
     <ul>
-      <li id="location" v-bind:class="(location===''||location==null?'not-available':'')">
-        <img
-          src="../assets/images/icon-location.svg"
-          alt="Location"
-          >
+      <li id="location" :class="(location===''||location==null?'not-available':'')">
+        <img src="../assets/images/icon-location.svg" alt="Location">
         <span>{{
           (location === "" || location == null?"Not Available":location)
         }}</span>
       </li>
-      <li id="website" v-bind:class="(website===''||website==null?'not-available':'')">
+      <li id="website" :class="(website===''||website==null?'not-available':'')">
         <img
           src="../assets/images/icon-website.svg"
           alt="Website"
@@ -19,16 +16,19 @@
           (website === "" || website == null?"Not Available":website)
         }}</span>
       </li>
-      <li  id="twitter" v-bind:class="(twitter===''||twitter==null?'not-available':'')">
+      <li  id="twitter" :class="(twitter===''||twitter==null?'not-available':'')">
         <img src="../assets/images/icon-twitter.svg" alt="Twitter">
         <span>{{
           (twitter === "" || twitter == null?"Not Available":`@${twitter}`)
         }}</span>
       </li>
-      <li id="company" v-bind:class="(company===''||company==null?'not-available':'')">
+      <li id="company" :class="(company===''||company==null?'not-available':'')">
         <img src="../assets/images/icon-company.svg" alt="Company">
-        <span>{{
-          (company === '' || company == null?'Not Available':`@${company}`)}}</span>
+        <span>
+          {{
+            (company === '' || company == null?'Not Available': `@${company}`)
+          }}
+        </span>
       </li>
     </ul>
   </div>
